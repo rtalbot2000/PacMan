ef9d5d48866f17ba222d94ebb81fee4fcd98558b
Ryan Talbot <ryan.talbot.829@k12.friscoisd.org>
2019-01-11 10:14:36 -0600

    Added rectangles

--- PacMan/PacMan/Game1.cs
+++ PacMan/PacMan/Game1.cs
@@ -19,9 +19,14 @@ namespace PacMan
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D spriteSheet, background;
+
         Rectangle[] Rectangles;
         Rectangle[] Origin;
         Rectangle backgroundRect;
+
+        Rectangle[] collisions;
+
+        MouseState oldMouse;
 
         public Game1()
         {
@@ -49,7 +54,53 @@ namespace PacMan
 
             };
 
-            
+            this.IsMouseVisible = true;
+
+            oldMouse = Mouse.GetState();
+
+            collisions = new Rectangle[]
+            {
+                new Rectangle(50, 0, 14, 246),
+                new Rectangle(50, 242, 156, 106),
+                new Rectangle(0, 397, 206, 107),
+                new Rectangle(50, 489, 15, 311),
+                new Rectangle(50, 0, 800, 12),
+                new Rectangle(433, 0, 32, 112),
+                new Rectangle(835, 0, 14, 256),
+                new Rectangle(692, 244, 158, 104),
+                new Rectangle(693, 397, 157, 107),
+                new Rectangle(835, 488, 15, 311),
+                new Rectangle(50, 784, 800, 16),
+                new Rectangle(119, 62, 87, 56),
+                new Rectangle(261, 62, 118, 56),
+                new Rectangle(520, 62, 116, 56),
+                new Rectangle(692, 62, 89, 56),
+                new Rectangle(20, 165, 186, 30),
+                new Rectangle(282, 165, 12, 182),
+                new Rectangle(290, 240, 89, 30),
+                new Rectangle(348, 166, 204, 29),
+                new Rectangle(433, 188, 33, 82),
+                new Rectangle(605, 166, 33, 182),
+                new Rectangle(520, 244, 117, 30),
+                new Rectangle(347, 321, 206, 106),
+                new Rectangle(261, 398, 32, 106),
+                new Rectangle(431, 474, 35, 107),
+                new Rectangle(347, 474, 204, 30),
+                new Rectangle(776, 630, 73, 32),
+                new Rectangle(50, 630, 72, 30),
+                new Rectangle(120, 552, 86, 31),
+                new Rectangle(178, 581, 28, 78),
+                new Rectangle(263, 552, 116, 30),
+                new Rectangle(519, 554, 116, 28),
+                new Rectangle(692, 553, 86, 28),
+                new Rectangle(692, 553, 30, 106),
+                new Rectangle(120, 708, 259, 28),
+                new Rectangle(262, 631, 30, 77),
+                new Rectangle(346, 631, 205, 30),
+                new Rectangle(432, 631, 35, 105),
+                new Rectangle(519, 708, 259, 28),
+                new Rectangle(605, 631, 31, 79)
+            };
 
             base.Initialize();
         }
@@ -89,6 +140,15 @@ namespace PacMan
                 this.Exit();
 
             // TODO: Add your update logic here
+            MouseState mouse = Mouse.GetState();
+
+            if(mouse.LeftButton == ButtonState.Pressed &&
+                oldMouse.LeftButton != ButtonState.Pressed)
+            {
+                Console.WriteLine(mouse.X + " " + mouse.Y);
+            }
+
+            oldMouse = mouse;
 
             base.Update(gameTime);
         }
@@ -104,6 +164,9 @@ namespace PacMan
             // TODO: Add your drawing code here
             spriteBatch.Begin();
             spriteBatch.Draw(background, backgroundRect, Color.White);
+            
+
+            
             spriteBatch.End();
 
             base.Draw(gameTime);
